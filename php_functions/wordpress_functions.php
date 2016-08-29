4bfeee10ef3d0bc786de1aac32f53d29b0b05d99
Joren De Graaf <jorendegraaf@gmail.com>
2016-08-29 11:04:49 +0200

    added filter_overview function

--- php_functions/wordpress_functions.php
+++ php_functions/wordpress_functions.php
@@ -11,4 +11,30 @@ function build_deeplink($base_url = null, $params = []){
   $deeplink = $base_url."?".$query_string;                  // Assumes trailing slash on base_url
   return $deeplink;
 }
+
+/**
+* Creates an human-readable overview of active filters.
+* Function was made to be able pass into the deeplink function.
+* ** Make this function more efficient in the future, it could be improved I think **
+* @param $active_filters Array with the active filters
+* @param $all_filters Array with all filters
+* @return Array or null
+**/
+function filter_overview($active_filters, $all_filters){
+  $overview = []; // Resulting array
+  if($active_filters): foreach($active_filters as $key => $value):
+    $taxonomy = $all_filters[$key]['taxonomy']; // store the current taxonomy
+    $terms = get_terms(['taxonomy' => $taxonomy, 'hide_empty' => false]); // get taxonomy terms for current filter
+    $overview[$key] = $all_filters[$key]['name'].":"; // assign the active filter's name to the result
+    if($terms): $i = 0;foreach($terms as $term):
+      if(in_array($term->slug, $value)): // check if the slug is in the active filters value array
+        $overview[$term->slug."_".$i] = "- ".$term->name; // then assign the active terms to the result
+       $i++;endif;
+   endforeach;endif;
+  endforeach;
+    return array_merge($overview); // return the merged array (maybe )
+  else: 
+    return null;
+  endif;
+}
 ?>
