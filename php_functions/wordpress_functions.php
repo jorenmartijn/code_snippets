bb0bf549d049cfdaab007f5373ec744f46cb6d02
Joren De Graaf <jorendegraaf@gmail.com>
2016-09-01 10:41:54 +0200

    updated deeplink function with return/echo toggle

--- php_functions/wordpress_functions.php
+++ php_functions/wordpress_functions.php
@@ -1,15 +1,18 @@
 <?php
 /**
- * Deeplink builder 
+ * Deeplink builder
  * @param $base_url string The base url, like site_url or an ACF Page Link
  * @param $params array Named array of parameter names and values, null values don't get merged.
+ * @param $echo boolean Switches between echo-ing and returning the result.
  * @return $deeplink The resulting deeplink
  */
-function build_deeplink($base_url = null, $params = []){
+function build_deeplink($base_url = null, $params = [], $echo = false){
   $base_url = (!is_null($base_url))? $base_url: site_url(); // Either site_url when null or the passed value
   $query_string = http_build_query( $params, "&amp;");      // merge the params into a query_string
   $deeplink = $base_url."?".$query_string;                  // Assumes trailing slash on base_url
-  return $deeplink;
+  if($echo == false): return $deeplink;
+  else: echo $deeplink;
+  endif;
 }
 
 /**
